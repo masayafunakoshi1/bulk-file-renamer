6e8c94b869000e304495c3617f3aaf83f1656581
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2022-09-15 17:05:48 -0400

    Added path name conditional

--- bulkFileRenamer.py
+++ bulkFileRenamer.py
@@ -4,27 +4,33 @@ def main():
     print("Welcome to the bulk-file-renamer! Using this script lets you rename all the files in a folder of your choosing!")
     print("It changes the name and indexes it starting from 0")
     print("e.g. Rename IMG3124.jpg --> Picture --> picture_0.jpg")
-    i = 0
+
     # for paths, make sure to use mnt/d
     print("Input path to folder: ")
-    path = validPathCheck()
+    path = input()
+    if(validPathCheck(path) != True): 
+        return
+
     print("Input edited name (spaces will be _): ")
     editedName = input()
+    
+
+    i = 0
     for filename in os.listdir(path):
         # if same name but capitalized, it will delete some items, be aware
-        editedName = "City_snaps_" + str(i) + ".jpg"
+        editedName = "temp" + str(i) + ".jpg"
         my_source =path + filename
         editedName =path +  editedName
         os.rename(my_source,  editedName)
         i += 1
 
-def validPathCheck():
-    temp = input()
-    if(os.path.exists(temp) != True):
-        print("Path not found.")
-        return
+def validPathCheck(path):
+    if(os.path.exists(path) != True):
+        print("Path set failed. Path not found.")
+        return False
     else:
-        return temp
+        print("Path set successfully.")
+        return True
 
 if __name__ == '__main__':
     main()
