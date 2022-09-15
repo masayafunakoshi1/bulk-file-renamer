6a41d36a71c84e3c12d9211dfe4bc373bfe2e321
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2022-09-15 16:23:56 -0400

    Adding inputs and instructions

--- bulkFileRenamer.py
+++ bulkFileRenamer.py
@@ -1,10 +1,15 @@
 import os
 
 def main():
+    print("Welcome to the bulk-file-renamer! Using this script lets you rename all the files in a folder of your choosing!")
+    print("It changes the name and indexes it starting from 0")
+    print("e.g. Rename IMG3124.jpg --> Picture --> picture_0.jpg")
     i = 0
     # for paths, make sure to use mnt/d
-    print("Input ")
-    path = "/mnt/d/Coding/Python/Learning/renamedFolder/"
+    print("Input path to folder: ")
+    path = input()
+    print("Input edited name (spaces will be _): ")
+    editedName = input()
     for filename in os.listdir(path):
         # if same name but capitalized, it will delete some items, be aware
         editedName = "City_snaps_" + str(i) + ".jpg"
@@ -14,8 +19,8 @@ def main():
         i += 1
 
 
+
 if __name__ == '__main__':
     main()
-
 
     
